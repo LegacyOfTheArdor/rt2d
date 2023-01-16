0f6ff8a0fdc3aa1a8018e8f69d1e3d14e984f0d8
LegacyOfTheArdor <95230814+LegacyOfTheArdor@users.noreply.github.com>
2023-01-16 11:52:00 +0100

    rotating movement

--- start/player.cpp
+++ start/player.cpp
@@ -11,21 +11,28 @@ Player::Player() : Entity()
 void Player::GoUp()
 {
     this->position.y = this->position.y - 32;
+    this->rotation.z = 0;
+	this->rotation.z = 1.5 * PI;
 }
 
 void Player::GoLeft()
 {
-   this->position.x = this->position.x - 32;     
+   this->position.x = this->position.x - 32; 
+   this->rotation.z = 0;
+   this->rotation.z = PI;      
 }
 
 void Player::GoDown()
 {
-   this->position.y = this->position.y + 32;
+   this->position.y = this->position.y + 32; 
+   this->rotation.z = 0;
+   this->rotation.z = 0.5 * PI; 
 }
 
 void Player::GoRight()
 {
     this->position.x = this->position.x + 32;
+    this->rotation.z = 0;     
 }
 
 Player::~Player()
